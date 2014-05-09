e33591b5185b651530531081197364a4379bbbd4
Vee Satayamas <vsatayamas@gmail.com>
2014-05-09 16:37:47 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+tha-eng-wn
+==========
+
+tha-eng-wn is Thai-English bidix generator from Wordnet
+
+## Prerequisite
+
+### SQLite3
+
+    gem install sqlite3
+
+### wNsql
+
+    ./download_wn.sh
---
+++ download_wn.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+wget -O wn_30_sqlite.db.zip https://sourceforge.net/projects/wnsql/files/wnsql3/sqlite/3.0/sqlite-30.db.zip/download
+unzip wn_30_sqlite.db.zip
+mv sqlite-30.db wn_30_sqlite.db
